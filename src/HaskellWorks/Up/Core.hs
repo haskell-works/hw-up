d529a0c95842cf75faffcc37cfaeba743b5f7691
John Ky <newhoggy@gmail.com>
2019-06-26 21:42:58 +1000

    New Task type

--- src/HaskellWorks/Up/Core.hs
+++ src/HaskellWorks/Up/Core.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
+
 module HaskellWorks.Up.Core where
 
+import Control.Selective (Selective (..))
+
+newtype Task k v = Task
+  { run :: forall f. Selective f => (k -> f v) -> f v
+  }
